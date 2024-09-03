c98ddabd5a605fe591353e13e3216f8d0b119a41
nicopekkanenHH <nico.pekkanen@myy.haaga-helia.fi>
2024-09-03 15:52:07 +0300

    Initial commit

---
+++ bookstore/backend/web/BookController.java
@@ -0,0 +1,16 @@
+package com.bookstore.backend.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        // This method will handle GET requests to /index
+        model.addAttribute("title", "Welcome to the Bookstore");
+        return "index";
+    }
+}
