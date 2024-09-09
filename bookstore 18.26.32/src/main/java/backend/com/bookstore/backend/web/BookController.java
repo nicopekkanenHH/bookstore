f8b631446a7500de6371bb043e27855b8419d1ed
nicopekkanenHH <165182826+nicopekkanenHH@users.noreply.github.com>
2024-09-09 21:04:49 +0300

    Update BookController.java

--- bookstore 18.26.32/src/main/java/backend/com/bookstore/backend/web/BookController.java
+++ bookstore 18.26.32/src/main/java/backend/com/bookstore/backend/web/BookController.java
@@ -9,7 +9,7 @@ public class BookController {
 
     @GetMapping("/index")
     public String index(Model model) {
-        // This method will handle GET requests to /index
+       
         model.addAttribute("title", "Welcome to the Bookstore");
         return "index";
     }
