4eb7f7cdb8e4cdb08ab6af3b4eb1051e86cff62a
nicopekkanenHH <165182826+nicopekkanenHH@users.noreply.github.com>
2024-09-09 21:31:17 +0300

    controller changes

--- bookstore 18.26.32/src/main/java/backend/com/bookstore/backend/web/BookController.java
+++ bookstore 18.26.32/src/main/java/backend/com/bookstore/backend/web/BookController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import backend.com.bookstore.backend.domain.Book;
 import backend.com.bookstore.backend.repository.BookRepository;
 
 @Controller
@@ -16,5 +20,20 @@ public class BookController {
     public String bookList(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
+    }
+     @GetMapping("/addbook")
+    public String showAddBookForm(Model model) {
+        model.addAttribute("book", new Book()); 
+        return "addbook";
+    }
+@PostMapping("/addbook")
+    public String addBook(@ModelAttribute Book book) {
+        bookRepository.save(book); 
+        return "redirect:/booklist"; 
+    }
+    @GetMapping("/deletebook/{isbn}")
+    public String deleteBook(@PathVariable("isbn") String isbn) {
+        bookRepository.deleteById(isbn); 
+        return "redirect:/booklist"; 
     }
 }
