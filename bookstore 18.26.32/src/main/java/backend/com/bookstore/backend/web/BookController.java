973948f1d66e51030a24dc2aeae032582b24255d
nicopekkanenHH <165182826+nicopekkanenHH@users.noreply.github.com>
2024-09-09 21:23:35 +0300

    booklist

--- bookstore 18.26.32/src/main/java/backend/com/bookstore/backend/web/BookController.java
+++ bookstore 18.26.32/src/main/java/backend/com/bookstore/backend/web/BookController.java
@@ -1,8 +1,11 @@
 package backend.com.bookstore.backend.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import backend.com.bookstore.backend.repository.BookRepository;
 
 @Controller
 public class BookController {
@@ -12,5 +15,12 @@ public class BookController {
        
         model.addAttribute("title", "Welcome to the Bookstore");
         return "index";
+    }
+    @Autowired
+    private BookRepository bookRepository;
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
     }
 }
