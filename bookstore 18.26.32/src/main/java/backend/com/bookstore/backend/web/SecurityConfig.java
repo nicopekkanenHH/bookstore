559940001f3361cc81079f39a87430a6aaec5a4e
nicopekkanenHH <165182826+nicopekkanenHH@users.noreply.github.com>
2024-09-25 18:43:47 +0300

    config2

--- bookstore 18.26.32/src/main/java/backend/com/bookstore/backend/web/SecurityConfig.java
+++ bookstore 18.26.32/src/main/java/backend/com/bookstore/backend/web/SecurityConfig.java
@@ -3,7 +3,7 @@ package backend.com.bookstore.backend.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
